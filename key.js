0a788f99f6191eb7f199247e09dbab0baf50e0a5
garameki <garameki@users.noreply.github.com>
2019-04-26 11:59:25 +0900

    Fin 	deleted:    compress.htm 	new file:   compruby.htm 	new file:   comptext.htm 	modified:   index.htm 	modified:   key.js 	modified:   ruby.js 	copied:     ruby.js -> ruby.js.original 	modified:   text.js 	modified:   text.js.original

--- key.js
+++ key.js
@@ -1,4 +1,7 @@
 (function(){
-keyletter = "ZnnZZs2ZuZoush3ZZkkuu2hiiZZgZjyoZrhoZm4Z5ZaisuaktsanZbu3oZi2aZ";
+keyletter = "ZnnZZs2ZuZoush3ZZkkuu2hiiZZgZjZryohoZm4ZZbsu5Zaitsakanu3i2oZaZ";
+key1 = "うYんYYしくYいYやうYじつYYに";
+key2 = "や0よY0しく22よゆYふYやYYら0じや3い2Yだみや2Y2や0にわ11じ1しくわYりむYじや";
+key3 = "さきせかとちごゆぐぜぎみじそはにがりしひだこぼぶわ";
 flagKey = true;
 })();
