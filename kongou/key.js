78a169df7f5ce4568bf42cddf14161c6aff9159c
garameki <garameki@users.noreply.github.com>
2019-05-10 16:48:15 +0900

    Add kongo/key.js 	new file:   key.js

---
+++ kongou/key.js
@@ -0,0 +1,7 @@
+(function(){
+keyletter = "ZnZsnZ2ZuZou3ZshZkkuu2oZZbsuai4ZhiiZtsaZyoZjZmi3ZgZdho7ZdaZieZ";
+key1 = "んYうYYしいYくYYにつYゆYよY";
+key2 = "や1ふYぼYだ3YだYさや47ぼ1しぜYがY27い20にYそ2やYら0しやYむY8らら3じやYじ";
+key3 = "せじさぶごにみとおふかぜこたちはきほいひぢくがけそ";
+flagKey = true;
+})();
