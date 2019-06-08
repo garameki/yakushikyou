88a4d6de5d0d5f1a5f314ea8fc8824123e5cab69
garameki <garameki@users.noreply.github.com>
2019-06-09 07:24:15 +0900

    Repair ruby and text of fumonbon 	modified:   key.js 	modified:   ruby.js 	modified:   ruby.js.original 	modified:   text.js 	modified:   text.js.original

--- fumonbon/key.js
+++ fumonbon/key.js
@@ -1,7 +1,11 @@
 (function(){
 keyletter = "2ZZsnZZnuZoushZk4Z3Zkuu2ZbhiZjZgsuintsiZZmkahoanyob2okZiZhu1Zo";
-key1 = "んYうYYしくYつYいYYじYにやう";
-key2 = "や1くわふYぜYYおわ02やYさ0に0しきYやYぼYYせ1し0ぜ0ぼYそさ4お0ゆYYらと320";
-key3 = "じおよむげりこだかいのやちゐぶどゆがたほにごみはせ";
+//keyletter = "2ZZsnZZnuZoushZk4Z3Zkuu2ZbhiZjZgsuintsiZZmkahoanyob2okZiZhu1Zo";
+//key1 = "んYうYYしくYつYいYYじYにやう";
+//key2 = "や1くわふYぜYYおわ02やYさ0に0しきYやYぼYYせ1し0ぜ0ぼYそさ4お0ゆYYらと320";
+//key3 = "じおよむげりこだかいのやちゐぶどゆがたほにごみはせ";
+key1 = "んYうYYしくYつYYにいYYじやう";
+key2 = "や1ふYわ0くわぜY0し0にやYYおYさ2や1しYせYそゆYぼYさ4きYYゐ0ぜお0Yく0ぼらY";
+key3 = "じよむくとげりぼこいだかちのやぶどゆがほたそきみご";
 flagKey = true;
 })();
